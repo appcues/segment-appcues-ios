45579b9ee54bcf3dc65e134045d9662087f66a60
Matt Hayashida <matt.hayashida@appcues.com>
2022-05-10 09:10:29 -0400

    🚚 Remove dash from package name

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Segment-Appcues",
+    name: "SegmentAppcues",
     platforms: [
         .iOS("13.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Segment-Appcues",
-            targets: ["Segment-Appcues"]),
+            name: "SegmentAppcues",
+            targets: ["SegmentAppcues"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,13 +26,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Segment-Appcues",
+            name: "SegmentAppcues",
             dependencies: [
                 "Segment",
                 .product(name: "AppcuesKit", package: "Appcues")
             ]),
         .testTarget(
-            name: "Segment-AppcuesTests",
-            dependencies: ["Segment-Appcues"]),
+            name: "SegmentAppcuesTests",
+            dependencies: ["SegmentAppcues"]),
     ]
 )
