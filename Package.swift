a896456098c26c5806cd970a99bbca5954c880b6
James Ellis <james@appcues.com>
2023-03-10 10:29:07 -0500

    ⬆️ Update Appcues dependency version to be a minimum 1.4.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Segment", url: "https://github.com/segmentio/analytics-swift.git" , from: "1.0.0"),
-        .package(name: "Appcues" , url: "https://github.com/appcues/appcues-ios-sdk.git", from: "1.3.0"),
+        .package(name: "Appcues" , url: "https://github.com/appcues/appcues-ios-sdk.git", from: "1.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
